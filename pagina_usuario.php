65ad45897681317349ff1ef0da2951d8750aa54d
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-06-04 10:51:44 -0500

    Se incluye el archivo informacion_productos

--- pagina_usuario.php
+++ pagina_usuario.php
@@ -11,19 +11,21 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.1.0/mdb.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="assets/styles.css">
 </head>
 
 <body>
+    <?php include("navbar.php"); ?>
     <section class="h-100 gradient-form" style="background-color: #eee;">
-        <div class="container py-5 h-100">
+        <div class="container py-5 h-1">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-xl-10">
+                <div class="col-xl-100">
                     <div class="card rounded-3 text-black">
                         <div class="row g-0">
-                            <div class="col-lg-6">
+                            <div class="col-lg-5">
                                 <div class="text-center">
-                                    <img src="assets/images/imagen_1.png" style="width: 185px;" alt="logo">
+                                    <img src="assets/images/imagen_1.png" style="width: 185px;" alt="logo UN">
                                     <br>
                                     <br>
                                     <br>
@@ -34,35 +36,8 @@
                                     <?php include("formulario_productos.php"); ?>
                                 </div>
                             </div>
-                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                                <table class="table align-middle table-dark">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Id</th>
-                                            <th scope="col">Nombre</th>
-                                            <th scope="col">Precio de venta</th>
-                                            <th scope="col">Imagen</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $query = "SELECT * FROM producto";
-                                        $result = mysqli_query($conn, $query);
-
-                                        while ($row = mysqli_fetch_array($result)) { ?>
-                                            <tr>
-                                                <td><?php echo $row['id'] ?></td>
-                                                <td><?php echo $row['nombre'] ?></td>
-                                                <td><?php echo $row['precio_venta'] ?></td>
-                                                <td>
-                                                    <img width="100" src="data:image/png;base64,<?php echo  base64_encode($row['imagen']); ?>">
-                                                </td>
-                                            </tr>
-
-                                        <?php } ?>
-
-                                    </tbody>
-                                </table>
+                            <div class="col-lg-7 d-flex align-items-center gradient-custom-2">
+                                <?php include('informacion_productos.php'); ?>
                             </div>
                         </div>
                     </div>
