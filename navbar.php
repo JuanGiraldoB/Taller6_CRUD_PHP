8a304d3d68a695e1e8001df41d208d162542b2bf
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-06-04 10:49:46 -0500

    navbar en archivo aparte para reutilizacion

---
+++ navbar.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-dark bg-dark">
+<div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Inicio</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
